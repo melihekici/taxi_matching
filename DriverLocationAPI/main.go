a5f111b215b0a9133e1f03b03c22feb29cc5954b
Melih Ekici <melih.ekici4@gmail.com>
2022-03-27 19:53:52 +0300

    middleware added for authentication

--- DriverLocationAPI/main.go
+++ DriverLocationAPI/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bitaksi/client"
 	"bitaksi/handlers"
+	"bitaksi/middleware"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,8 +18,8 @@ func main() {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("asd")
 	})
-	mux.Handle("/drivers", handlers.DriverHandler)
-	mux.Handle("/drivers/", handlers.DriverHandler)
+	mux.Handle("/drivers", middleware.TokenValidationMiddleware(handlers.DriverHandler))
+	mux.Handle("/drivers/", middleware.TokenValidationMiddleware(handlers.DriverHandler))
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
