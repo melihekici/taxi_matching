d5acd9cb76cfecd704a926498ab89427fadcf6db
Melih Ekici <melih.ekici4@gmail.com>
2022-03-28 23:44:39 +0300

    commented old code removed

--- MatchingAPI/algorithms/distance.go
+++ MatchingAPI/algorithms/distance.go
@@ -14,16 +14,8 @@ func Haversine(firstLocation models.Location, secondLocation models.Location) fl
 	lat1, long1 := degreeToRadians(firstLocation.Coordinates[0]), degreeToRadians(firstLocation.Coordinates[1])
 	lat2, long2 := degreeToRadians(secondLocation.Coordinates[0]), degreeToRadians(secondLocation.Coordinates[1])
 
-	// absLatDifference := math.Abs(lat1 - lat2)
 	absLongDifference := math.Abs(long1 - long2)
 	centralAngle := math.Acos(math.Sin(lat1)*math.Sin(lat2) + math.Cos(lat1)*math.Cos(lat2)*math.Cos(absLongDifference))
-
-	// a := math.Pow(math.Sin(absLatDifference/2), 2)
-	// b := math.Pow(math.Sin((lat1-lat2)/2), 2)
-	// c := math.Pow(math.Sin(absLongDifference/2), 2)
-
-	// centralAngle := 2 * math.Asin(math.Sqrt(
-	// 	a+(1-a-b)*c))
 
 	return EARTH_RAIDUS * centralAngle
 }
