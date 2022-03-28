3354369c8b02cf8bc184209a4109339e7a813200
Melih Ekici <melih.ekici4@gmail.com>
2022-03-28 23:47:55 +0300

    FindDrivers method implemented

--- MatchingAPI/controllers/matching.go
+++ MatchingAPI/controllers/matching.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"matching/models"
+	"matching/services"
 	"net/http"
 )
 
@@ -17,6 +18,11 @@ type findDriversRequest struct {
 	Radius   float64         `json:"radius"` // in meters
 }
 
+type findDriversResponse struct {
+	Driver   models.Driver `json:"driver"`
+	Distance float64       `json:"distance"`
+}
+
 func (m *matchingController) FindDrivers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Looking for drivers")
 
@@ -24,11 +30,37 @@ func (m *matchingController) FindDrivers(w http.ResponseWriter, r *http.Request)
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Bad Request"))
+		w.Write([]byte("Bad Request. " + err.Error()))
 		return
 	}
 
-	var driverList []models.Driver
-	
+	driverList, err := services.GetAllDrivers(w.Header().Get("Token"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error. " + err.Error()))
+		return
+	}
 
+	if len(driverList) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("No drivers nearby."))
+		return
+	}
+
+	closestDriver, distance := services.FindClosestDriverInRadius(req.Location, req.Radius, driverList)
+	if closestDriver == nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("No drivers nearby."))
+		return
+	}
+
+	resp := findDriversResponse{Driver: *closestDriver, Distance: distance}
+	responseBody, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to serialize drivers list. " + err.Error()))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseBody)
 }
