599ed9234f2b81ad046e5415710464ea228a9b3d
Melih Ekici <melih.ekici4@gmail.com>
2022-03-29 23:43:02 +0300

    reading config from env

--- DriverLocationAPI/config/config.go
+++ DriverLocationAPI/config/config.go
@@ -1,23 +1,24 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
+// var MONGO = map[string]string{
+// 	"URI":            "mongodb://localhost:27017",
+// 	"DATABASE":       "bitaksi",
+// 	"COLLECTION":     "driver",
+// 	"TESTCOLLECTION": "test",
+// }
+
+// var SECRET = "^S+%R&YUI&G/H(J/H)("
+
 var MONGO = map[string]string{
-	"URI":            "mongodb://mongodb:27017",
-	"DATABASE":       "bitaksi",
-	"COLLECTION":     "driver",
-	"TESTCOLLECTION": "test",
+	"URI":            fmt.Sprintf("mongodb://%s:27017", os.Getenv("MONGO_HOST")),
+	"DATABASE":       os.Getenv("DATABASE"),
+	"COLLECTION":     os.Getenv("COLLECTION"),
+	"TESTCOLLECTION": os.Getenv("TESTCOLLECTION"),
 }
 
-var SECRET = "^S+%R&YUI&G/H(J/H)("
-
-// func init() {
-// 	file, _ := os.Open("config/conf.json")
-// 	defer file.Close()
-// 	decoder := json.NewDecoder(file)
-// 	config := configuration{}
-// 	err := decoder.Decode(&config)
-// 	if err != nil {
-// 		log.Fatalln("Error reading configuration.", err.Error())
-// 	}
-
-// 	MONGO = config.MONGO
-// }
+var SECRET = os.Getenv("SECRET")
