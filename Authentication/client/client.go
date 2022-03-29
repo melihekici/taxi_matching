3ffd3ad149ad90f49d7776e30d06970390668278
Melih Ekici <melih.ekici4@gmail.com>
2022-03-29 23:35:27 +0300

    read postgres host from env

--- Authentication/client/client.go
+++ Authentication/client/client.go
@@ -4,25 +4,24 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func PostgresConnection() *sql.DB {
-	// postgresInfo := fmt.Sprintf(
-	// 	"host =%s port=%d user=%s password=%s dbname=%s sslmode=disable", "localhost", 5438, "postgres", "postgres", "postgres")
-
-	url := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", "postgres", "postgres", "localhost", 5438, "postgres")
+	url := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", "postgres", "postgres", os.Getenv("POSTGRES_HOST"), 5432, "postgres")
 
 	db, err := sql.Open("postgres", url)
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	log.Panicln(err)
-	// }
+	err = db.Ping()
+	if err != nil {
+		log.Panicln("Unable to connect to postgres database " + err.Error())
+	}
 
+	log.Println("Connected to Postgres Database")
 	return db
 }
