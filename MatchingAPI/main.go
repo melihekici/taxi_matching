a0413c8fec3d3fb9d33473a53c9534fc39b31ba6
Melih Ekici <melih.ekici4@gmail.com>
2022-04-02 13:30:35 +0300

    new handler added for swagger documentation

--- MatchingAPI/main.go
+++ MatchingAPI/main.go
@@ -5,12 +5,20 @@ import (
 	"matching/handlers"
 	"matching/middleware"
 	"net/http"
+
+	openApiMiddleware "github.com/go-openapi/runtime/middleware"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/find", middleware.TokenValidationMiddleware(handlers.MatchingHandler))
+
+	// documentation
+	opts1 := openApiMiddleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	sh := openApiMiddleware.Redoc(opts1, nil)
+	mux.Handle("/docs", sh)
+	mux.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
 	err := http.ListenAndServe(":9191", mux)
 	if err != nil {
