70d537dff56cb4e5972262310ec5d8109a2040d0
Melih Ekici <melih.ekici4@gmail.com>
2022-04-03 18:27:00 +0300

    driverapihealthcheck is moved to driverapi.go file

--- MatchingAPI/services/driver.go
+++ MatchingAPI/services/driver.go
@@ -4,23 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 	"matching/algorithms"
 	"matching/models"
 	"net/http"
 	"os"
 )
-
-func DriverApiHealthCheck() bool {
-	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:8080/health", os.Getenv("DRIVER_API_HOST")), nil)
-	if err != nil {
-		log.Println("error consturcting health check request")
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	return err == nil && resp.StatusCode == 200
-}
 
 func GetAllDrivers(token string) ([]models.Driver, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:8080/drivers", os.Getenv("DRIVER_API_HOST")), nil)
