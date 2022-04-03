2d2b67ebb63e89e8b4b75357fa47ff5f7c2e7d7e
Melih Ekici <melih.ekici4@gmail.com>
2022-04-03 18:24:32 +0300

    driver api documentation is added to matching api

--- MatchingAPI/main.go
+++ MatchingAPI/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"matching/handlers"
 	"matching/middleware"
+	"matching/services"
 	"net/http"
 
 	openApiMiddleware "github.com/go-openapi/runtime/middleware"
@@ -19,6 +20,12 @@ func main() {
 	sh := openApiMiddleware.Redoc(opts1, nil)
 	mux.Handle("/docs", sh)
 	mux.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+	// driver-api-documentation
+	opts2 := openApiMiddleware.RedocOpts{SpecURL: "/driver-api.yaml", Path: "driver-api-docs"}
+	sh2 := openApiMiddleware.Redoc(opts2, nil)
+	mux.Handle("/driver-api-docs", sh2)
+	mux.HandleFunc("/driver-api.yaml", services.GetDriverApiDocs)
 
 	err := http.ListenAndServe(":9191", mux)
 	if err != nil {
